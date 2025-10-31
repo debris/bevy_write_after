093fbea68b5be80b8187aa39e197e9bd05f5abf6
Marek Kotewicz <marek.kotewicz@gmail.com>
2025-10-31 13:56:43 +0200

    added GlobalMessagePool && MessagePoolEmptied

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@
 //! #[derive(Message)]
 //! struct MyMessage;
 //!
+//! #[derive(Component)]
+//! struct MyPool;
+//!
 //! fn my_main() {
 //!     App::new()
 //!         .add_message::<MyMessage>()
@@ -17,10 +20,13 @@
 //! }
 //!
 //! fn setup(mut commands: Commands) {
-//!     commands.spawn(MessagePool::default());
+//!     commands.spawn((
+//!         MessagePool::default(),
+//!         MyPool,
+//!     ));
 //! }
 //!
-//! fn some_system(mut pool: Single<&mut MessagePool>) {
+//! fn some_system(mut pool: Single<&mut MessagePool, With<MyPool>>) {
 //!     pool.write_after(MyMessage, 1.0);
 //! }
 //!
@@ -35,7 +41,10 @@ pub struct WriteAfterPlugin;
 
 impl Plugin for WriteAfterPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, process_messages);
+        app
+            .add_message::<MessagePoolEmptied>()
+            .add_systems(Startup, spawn_global_message_pool)
+            .add_systems(Update, process_messages);
     }
 }
 
@@ -44,9 +53,20 @@ struct QueuedMessage {
     write_fn: Box<dyn FnOnce(&mut Commands) + Send + Sync + 'static>,
 }
 
+type CustomEmptiedMessage = Box<dyn Fn(&mut Commands, Entity) + Send + Sync + 'static>;
+
+/// Message sent when the pool is empty.
+#[derive(Message)]
+pub struct MessagePoolEmptied(pub Entity);
+
+/// Global message pool.
+#[derive(Component)]
+pub struct GlobalMessagePool;
+
 #[derive(Component, Default)]
 pub struct MessagePool {
     messages: Vec<QueuedMessage>,
+    when_emptied: Option<CustomEmptiedMessage>,
 }
 
 impl MessagePool {
@@ -54,25 +74,49 @@ impl MessagePool {
         let timer = Timer::from_seconds(delay, TimerMode::Once);
 
         let write_fn = Box::new(move |commands: &mut Commands| {
-            commands.queue(|world: &mut World| {
-                let mut writer = world.resource_mut::<Messages<M>>();
-                writer.write(message);
+            commands.queue(move |world: &mut World| {
+                world.resource_mut::<Messages<M>>().write(message);
             });
         });
 
         self.messages.push(QueuedMessage { timer, write_fn });
     }
+
+    pub fn write_when_empty<M: Message + Send + Sync + Clone + 'static>(&mut self, message: M) {
+        let write_fn = Box::new(move |commands: &mut Commands, emptied: Entity| {
+            let message = message.clone();
+            commands.queue(move |world: &mut World| {
+                world.resource_mut::<Messages<MessagePoolEmptied>>().write(MessagePoolEmptied(emptied));
+                world.resource_mut::<Messages<M>>().write(message);
+            });
+        });
+
+        self.when_emptied = Some(write_fn);
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.messages.is_empty()
+    }
+}
+
+fn spawn_global_message_pool(
+    mut commands: Commands
+) {
+    commands.spawn((
+        MessagePool::default(),
+        GlobalMessagePool,
+    ));
 }
 
 fn process_messages(
     mut commands: Commands,
     time: Res<Time>,
-    query: Query<&mut MessagePool>,
+    query: Query<(Entity, &mut MessagePool)>,
 ) {
-    for mut queue in query {
+    for (entity, mut pool) in query {
         let mut finished = Vec::new();
 
-        for (i, message) in queue.messages.iter_mut().enumerate() {
+        for (i, message) in pool.messages.iter_mut().enumerate() {
             message.timer.tick(time.delta());
             if message.timer.is_finished() {
                 finished.push(i);
@@ -80,8 +124,11 @@ fn process_messages(
         }
 
         for i in finished.into_iter().rev() {
-            let message = queue.messages.remove(i);
+            let message = pool.messages.remove(i);
             (message.write_fn)(&mut commands);
+            if let Some(ref when_empty) = pool.when_emptied && pool.messages.is_empty() {
+                (when_empty)(&mut commands, entity);
+            }
         }
         
     }
@@ -99,12 +146,6 @@ mod tests {
 
     #[test]
     fn test_message_after() {
-        fn setup(
-            mut commands: Commands
-        ) {
-            commands.spawn(MessagePool::default());
-        }
-
         fn add_message_hello(
             mut after: Single<&mut MessagePool, Added<MessagePool>>,
         ) {
@@ -121,7 +162,6 @@ mod tests {
         app.add_message::<TestMessage>();
         app.init_resource::<Time>();
         app.add_plugins(WriteAfterPlugin);
-        app.add_systems(Startup, setup);
         app.add_systems(Update, add_message_hello);
         app.add_systems(Update, add_message_hello2);
         app.update();
